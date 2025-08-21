c11a57179deea2c398fb4831822cf5def7eb1e49
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-20 20:18:51 -0400

    implement activity concept

---
+++ src/activities/ActivityDetailsPage.jsx
@@ -0,0 +1,70 @@
+import { useNavigate, useParams } from "react-router";
+import useQuery from "../api/useQuery";
+import { useAuth } from "../auth/AuthContext";
+import useMutation from "../api/useMutation";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function ActivityDetailsPage() {
+  const { id } = useParams(); //Grab the id
+  const { token } = useAuth();
+  const navigate = useNavigate();
+  const [deleteCalled, setDeleteCalled] = useState(false); //to track if delete button is clicked
+
+  //Delete an activity per given id
+  const {
+    mutate: deleteActivity,
+    loading: deleteLoading,
+    error: deleteError,
+  } = useMutation("DELETE", "/activities/" + id, ["activities"]);
+
+  //Fetch a particular activity per given id
+  const {
+    data: activity,
+    loading: fetchLoading,
+    error: fetchError,
+  } = useQuery(`/activities/${id}`, "activities");
+
+  /**
+   * Added a useEffect to navigate after deletion is successful
+   * If delete button clicked, loading completed and no delete errors,
+   * navigate to activities page.
+   */
+  useEffect(() => {
+    if (!deleteLoading && !deleteError && deleteCalled) {
+      navigate("/");
+    }
+  }, [deleteLoading, deleteError, navigate, deleteCalled]);
+
+  // If still fetching OR no activity fetched, display Loading
+  if (fetchLoading || !activity) return <p>Loading...</p>;
+  return (
+    <>
+      <h1>Activity Details</h1>
+      <p>
+        <strong>Activity Name: </strong>
+        {activity.name}
+      </p>
+      <p>
+        <strong>Activity Description: </strong>
+        {activity.description}
+      </p>
+      <p>
+        <strong>Creator Name: </strong>
+        {activity.creatorName}
+      </p>
+      {token && (
+        <button
+          onClick={() => {
+            setDeleteCalled(true);
+            deleteActivity();
+          }}
+        >
+          {deleteLoading ? "Deleting" : deleteError ? deleteError : "Delete"}
+        </button>
+      )}
+
+      {fetchError && <output>{fetchError}</output>}
+    </>
+  );
+}
